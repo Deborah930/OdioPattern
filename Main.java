34c07875053fc4edf139fee2360167043a409b2b
heimdall-exe <francesco.pierno000@gmail.com>
2025-07-11 17:23:14 +0200

    skibidi

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+//SONO STATO UTILE        
+GameEngineFacade engine = new GameEngineFacade();
+        engine.avvioGioco();
+    }
+}
